763ad2ea663ee1524a6c771ab7539e6684692d39
Rafał Gawlik <rafal.maly@gmail.com>
2015-08-31 13:29:57 +0200

    handle sass/css add dev/prod webpack configs

--- lib/generators/react_webpack_rails/install_generator.rb
+++ lib/generators/react_webpack_rails/install_generator.rb
@@ -4,10 +4,14 @@ module ReactWebpackRails
 
     def generate_layout
       copy_file 'webpack.config.js', 'webpack.config.js'
+      copy_file 'dev.config.js', 'webpack/dev.config.js'
+      copy_file 'production.config.js', 'webpack/production.config.js'
       copy_file 'karma.conf.js', 'karma.conf.js'
-      copy_file 'tests.webpack.js', 'tests.webpack.js'
+      copy_file 'tests.config.js', 'webpack/tests.config.js'
       copy_file 'index.js', 'app/react/index.js'
       copy_file 'hello-world.jsx', 'app/react/components/hello-world.jsx'
+      copy_file 'hello-world-test.jsx', 'app/react/components/hello-world-test.jsx'
+
       template 'package.json.erb', 'package.json'
     end
 
