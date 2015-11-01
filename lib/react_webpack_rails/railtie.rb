cc18af55e3372da17ab59fb6db8806a83bf34045
Radosław Piątek <radoslaw.piatek@netguru.pl>
2015-11-01 20:00:55 +0100

    Add guard for engine and railtie to load only with Rails

--- lib/react_webpack_rails/railtie.rb
+++ lib/react_webpack_rails/railtie.rb
@@ -1,7 +1,7 @@
 require 'react_webpack_rails/view_helpers'
 
 module ReactWebpackRails
-  class Railtie < Rails::Railtie
+  class Railtie < ::Rails::Railtie
     initializer 'react_webpack_rails.view_helpers' do
       ActionView::Base.send :include, ViewHelpers
     end
