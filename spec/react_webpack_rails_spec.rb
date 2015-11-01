a1835dd23e10c2e42ce0db9ce247031687be089e
Radosław Piątek <radoslaw.piatek@netguru.pl>
2015-11-01 22:30:01 +0100

    Add tests for helpers

--- spec/react_webpack_rails_spec.rb
+++ spec/react_webpack_rails_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe ReactWebpackRails do
+RSpec.describe ReactWebpackRails do
   it 'has a version number' do
     expect(ReactWebpackRails::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
