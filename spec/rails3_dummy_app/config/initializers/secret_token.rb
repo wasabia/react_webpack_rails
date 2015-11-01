c67202904dece99aeb09ae7839826aeaa4ade44a
Radosław Piątek <radoslaw.piatek@netguru.pl>
2015-11-01 20:31:59 +0100

    Add dummy rails3 application as tests base

---
+++ spec/rails3_dummy_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3DummyApp::Application.config.secret_token = '296751c0f72265270820caecebf59497dfc23dfbcbc8d9521ebf879efd380f60943c3fd4f90ccb05e6eb4c538765308cfecf7b113bfa416d5e0a77a746c6cd7e'
