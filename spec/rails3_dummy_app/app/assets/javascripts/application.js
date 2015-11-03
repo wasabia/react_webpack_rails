c26e56168a27f68a8de0e8eb595ffc5c4cf50c3f
marcinkornek <marcinkornek@gmail.com>
2015-11-03 18:57:05 +0100

    Add 'react_webpack_rails' gem to dummy app

--- spec/rails3_dummy_app/app/assets/javascripts/application.js
+++ spec/rails3_dummy_app/app/assets/javascripts/application.js
@@ -10,3 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require react_integration
+//= require react_bundle
