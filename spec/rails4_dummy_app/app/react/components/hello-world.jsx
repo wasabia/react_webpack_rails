6e8953db70e8bf419dd563e41cfe9ad5eeb4bd91
marcinkornek <marcinkornek@gmail.com>
2015-11-04 15:55:21 +0100

    Add 'name' props to hello-world react component, add component to client_side_hello_world view in 'rails4_dummy_app'

--- spec/rails4_dummy_app/app/react/components/hello-world.jsx
+++ spec/rails4_dummy_app/app/react/components/hello-world.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Users extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    return(
+      <div>
+        Hello World {this.props.name}
+      </div>
+    );
   }
 }
 
