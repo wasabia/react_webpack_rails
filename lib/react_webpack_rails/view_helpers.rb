3188257d83b6ed6d638f095706c38451682bc02c
Rafał Gawlik <rafal.maly@gmail.com>
2015-11-25 23:58:59 +0100

    replace flags with element type

--- lib/react_webpack_rails/view_helpers.rb
+++ lib/react_webpack_rails/view_helpers.rb
@@ -1,12 +1,12 @@
 module ReactWebpackRails
   module ViewHelpers
     # based on https://github.com/reactjs/react-rails/blob/master/lib/react/rails/view_helper.rb
-    def react_component(name, props = {}, options = {}, &block)
+    def react_element(name, element_type, props = {}, options = {}, &block)
       html_options = options.reverse_merge(data: {})
       html_options[:data].tap do |data|
         data[:react_class] = name
         data[:react_props] = (props.is_a?(String) ? props : props.to_json)
-        data[:react_router] = options.delete(:react_router)
+        data[:element_type] = element_type
       end
       html_tag = html_options[:tag] || :div
       html_options.except!(:tag)
@@ -14,8 +14,12 @@ module ReactWebpackRails
       content_tag(html_tag, '', html_options, &block)
     end
 
+    def react_component(name, props = {}, options = {})
+      react_element(name, :component, props, options)
+    end
+
     def react_router(name)
-      react_component(name, {}, react_router: true)
+      react_element(name, :router)
     end
   end
 end
