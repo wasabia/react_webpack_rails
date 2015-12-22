61644cd03726d1db9b12f5bdf7248b6223f1a9ed
Rafał Gawlik <rafal.maly@gmail.com>
2015-12-23 00:59:49 +0100

    cleanup react_element helper

--- lib/react_webpack_rails/view_helpers.rb
+++ lib/react_webpack_rails/view_helpers.rb
@@ -2,15 +2,14 @@ module ReactWebpackRails
   module ViewHelpers
     # based on https://github.com/reactjs/react-rails/blob/master/lib/react/rails/view_helper.rb
     def react_element(integration_name, payload = {}, options = {}, &block)
-      html_options = options.reverse_merge(data: {})
+      html_options = { data: {} }
       html_options[:data].tap do |data|
-        data[:react_element] = true
         data[:integration_name] = integration_name
-        data[:payload] = payload
-        data[:options] = options
+        data[:options] = options.except(:tag)
+        data[:payload] = (payload.is_a?(String) ? payload : payload.to_json)
+        data[:react_element] = true
       end
-      html_tag = html_options[:tag] || :div
-      html_options.except!(:tag)
+      html_tag = options[:tag] || :div
 
       content_tag(html_tag, '', html_options, &block)
     end
