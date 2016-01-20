f42ebaa18d34c3590ddc8ab65c55ceed099e6e8a
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2016-01-20 17:11:38 +0100

    Expand test react component

--- spec/rails4_dummy_app/app/react/components/hello-world.jsx
+++ spec/rails4_dummy_app/app/react/components/hello-world.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.state = {count: 1};
+  }
+
+ onClick() {this.setState({count: this.state.count + 1})}
+
   render() {
     return(
       <div>
         Hello World {this.props.name}
+        <button onClick={this.onClick}>count up</button>
+        {this.state.count}
       </div>
     );
   }
