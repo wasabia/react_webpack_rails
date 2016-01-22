5187d4a8bf1aad474b47bf63902717847c5b3a56
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2016-01-22 11:40:57 +0100

    Rescue node failure on rails side (WIP)

--- lib/react_webpack_rails/node_renderer.rb
+++ lib/react_webpack_rails/node_renderer.rb
@@ -9,9 +9,11 @@ module ReactWebpackRails
     end
 
     def call
-      Net::HTTP.start(node_uri.host, node_uri.port) do |http|
+      @response = Net::HTTP.start(node_uri.host, node_uri.port) do |http|
         http.request(request)
       end
+    rescue => e
+      raise Errors::NodeFailure, e.to_s # error_data
     end
 
     private
@@ -28,6 +30,11 @@ module ReactWebpackRails
       request = Net::HTTP::Post.new(node_uri)
       request.body = data_hash.to_json
       request
+    end
+
+    def error_data
+      'error data'
+      # JSON.parse(@response.body).slice('error', 'error_description').to_s
     end
   end
 end
