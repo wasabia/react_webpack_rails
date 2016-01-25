3b1f1940fcf5ab17c883ce46323f81c555419492
Radosław Piątek <radoslaw.piatek@netguru.pl>
2016-01-25 11:22:25 +0100

    Fix react tests and install newer npm on travis

--- spec/rails3_dummy_app/spec/helpers/react_webpack_rails/view_helpers_spec.rb
+++ spec/rails3_dummy_app/spec/helpers/react_webpack_rails/view_helpers_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ReactWebpackRails::ViewHelpers, type: :helper do
     it 'renders html tag' do
       expect(subject).to be_an_instance_of(ActiveSupport::SafeBuffer)
       expect(subject).to eq(
-        "<div data-integration-name=\"react-component\" data-options=\"{}\" "\
+        "<div data-integration-name=\"react-component\" "\
         "data-payload=\"{}\" data-react-element=\"true\"></div>"
       )
     end
@@ -30,24 +30,24 @@ RSpec.describe ReactWebpackRails::ViewHelpers, type: :helper do
     end
 
     context 'when options without tag given' do
-      subject { helper.react_element('react-component', {}, {foo: :bar}) }
+      subject { helper.react_element('react-component', {foo: :bar}) }
       it 'is renders div' do
         expect(subject).to include('<div ', '></div>')
       end
 
       it 'pass given options' do
-        expect(subject).to include("data-options=\"{&quot;foo&quot;:&quot;bar&quot;}\"")
+        expect(subject).to include("data-payload=\"{&quot;foo&quot;:&quot;bar&quot;}\"")
       end
     end
 
     context 'when options with tag given' do
-      subject { helper.react_element('react-component', {}, {foo: :bar}, {tag: :li}) }
+      subject { helper.react_element('react-component', {foo: :bar}, {tag: :li}) }
       it 'is renders passed tag' do
         expect(subject).to include('<li ', '></li>')
       end
 
       it 'pass given options without tag' do
-        expect(subject).to include("data-options=\"{&quot;foo&quot;:&quot;bar&quot;}\"")
+        expect(subject).to include("data-payload=\"{&quot;foo&quot;:&quot;bar&quot;}\"")
       end
     end
   end
@@ -58,7 +58,7 @@ RSpec.describe ReactWebpackRails::ViewHelpers, type: :helper do
     it 'wraps #react_component with proper options' do
       expect(helper)
         .to receive(:react_element)
-        .with('react-component', { foo: 'bar' }, {name: 'Todo'}, {})
+        .with('react-component', { props: { foo: 'bar' }, name: 'Todo' }, {ssr: false})
         .once
       helper.react_component('Todo', foo: 'bar')
     end
@@ -67,7 +67,9 @@ RSpec.describe ReactWebpackRails::ViewHelpers, type: :helper do
       subject { helper.react_component('Todo') }
 
       it 'sets an empty object as default' do
-        expect(helper).to receive(:react_element).with('react-component', {}, {name: 'Todo'}, {}).once
+        expect(helper).to receive(:react_element).with(
+          'react-component', { props: {}, name: 'Todo' }, { ssr: false }
+        ).once
         helper.react_component('Todo')
       end
     end
@@ -77,7 +79,7 @@ RSpec.describe ReactWebpackRails::ViewHelpers, type: :helper do
     it { expect(helper).to respond_to(:react_router) }
 
     it 'wraps #react_component with proper options' do
-      expect(helper).to receive(:react_element).with('react-router', {}, name: 'TodoRouter').once
+      expect(helper).to receive(:react_element).with('react-router', name: 'TodoRouter').once
       helper.react_router('TodoRouter')
     end
   end
