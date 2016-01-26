1525d11322d84c166d16abba4f3e186e7353e716
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2016-01-26 22:57:56 +0100

    Add node server dependencies to package.json

--- lib/generators/react_webpack_rails/install_generator.rb
+++ lib/generators/react_webpack_rails/install_generator.rb
@@ -5,25 +5,25 @@ module ReactWebpackRails
     class_option :example, type: :boolean, default: true, desc: 'Include example component and test files.'
     class_option :router, type: :boolean, default: true, desc: 'Add and expose react-router globally.'
 
-
     def generate_layout
       copy_file '.babelrc', '.babelrc'
+      copy_file 'karma.conf.js', 'karma.conf.js'
+      copy_file 'node_server.js', 'node_server.js'
+      copy_file 'partial/_react_hot_assets.html.erb', 'app/views/layouts/_react_hot_assets.html.erb'
       copy_file 'webpack.config.js', 'webpack.config.js'
       copy_file 'webpack/dev.config.js', 'webpack/dev.config.js'
       copy_file 'webpack/hot-dev.config.js', 'webpack/hot-dev.config.js'
       copy_file 'webpack/production.config.js', 'webpack/production.config.js'
-      copy_file 'karma.conf.js', 'karma.conf.js'
       copy_file 'webpack/tests.config.js', 'webpack/tests.config.js'
-      copy_file 'partial/_react_hot_assets.html.erb', 'app/views/layouts/_react_hot_assets.html.erb'
-      template 'react/index.js.erb', 'app/react/index.js'
       create_file 'app/assets/javascripts/react_bundle.js'
+      template 'package.json.erb', 'package.json'
+      template 'react/index.js.erb', 'app/react/index.js'
       if options.example
-        copy_file 'react/components/hello-world.jsx', 'app/react/components/hello-world.jsx'
         copy_file 'react/components/hello-world-test.jsx', 'app/react/components/hello-world-test.jsx'
+        copy_file 'react/components/hello-world.jsx', 'app/react/components/hello-world.jsx'
       else
         create_file 'app/react/components/.keep'
       end
-      template 'package.json.erb', 'package.json'
     end
 
     private
