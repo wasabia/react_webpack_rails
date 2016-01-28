5d413f36699da0d18be74c53fe33aa232d82ff29
Rafał Gawlik <rafal.maly@gmail.com>
2016-01-28 17:42:06 +0100

    keep hello-world simple

--- lib/generators/react_webpack_rails/templates/react/components/hello-world.jsx
+++ lib/generators/react_webpack_rails/templates/react/components/hello-world.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 
 class HelloWorld extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onClick = this.onClick.bind(this);
-    this.state = {count: 1};
-  }
-
- onClick() {this.setState({count: this.state.count + 1})}
-
   render() {
     return(
       <div>
