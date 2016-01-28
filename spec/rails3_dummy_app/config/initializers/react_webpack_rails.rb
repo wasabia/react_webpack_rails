28d8b1d16fbd2575418583e267ff60f246ee3457
Rafał Gawlik <rafal.maly@gmail.com>
2016-01-28 17:44:56 +0100

    regenerate/update dummy apps

--- spec/rails3_dummy_app/config/initializers/react_webpack_rails.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.configure do
-  # set node server host
-  # config.rwr.node_server_host = 'http://localhost:8081/'
-
-  # enable/disable server side render globally
-  # config.react.server_side = false
-end
--- spec/rails4_dummy_app/config/initializers/react_webpack_rails.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.configure do
-  config.react.node_server = 'http://localhost:8080/'
-  config.react.server_side = false
-end
