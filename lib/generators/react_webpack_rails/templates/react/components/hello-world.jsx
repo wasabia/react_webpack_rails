57211a325a0d882e4352069240c7530eb2be73cd
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2016-01-29 13:48:24 +0100

    Add state read in hello world react template

--- lib/generators/react_webpack_rails/templates/react/components/hello-world.jsx
+++ lib/generators/react_webpack_rails/templates/react/components/hello-world.jsx
@@ -4,7 +4,7 @@ class HelloWorld extends React.Component {
   render() {
     return(
       <div>
-        <p>Hello World</p>
+        <p>Hello World {this.props.name}</p>
       </div>
     );
   }
