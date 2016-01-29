3c3b598eef07e4eb8c7b5c84d0bad4a5d3b931c8
Rafał Gawlik <rafal.maly@gmail.com>
2016-01-29 01:08:55 +0100

    extract router integration out from core package

--- js/src/integrations-manager.js
+++ js/src/integrations-manager.js
@@ -1,11 +1,9 @@
 import reactIntegration from './integrations/react';
-import reactRouterIntegration from './integrations/react-router';
 
 class IntegrationsManager {
   constructor() {
     this.integrations = {
       'react-component': reactIntegration.integrationWrapper,
-      'react-router': reactRouterIntegration.integrationWrapper,
     };
   }
 
