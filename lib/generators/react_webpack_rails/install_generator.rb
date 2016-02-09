af91da36e30c96f16da3750537a2dab7a245ae4d
Rafał Gawlik <rafal.maly@gmail.com>
2016-02-09 10:32:19 +0100

    class_options done right

--- lib/generators/react_webpack_rails/install_generator.rb
+++ lib/generators/react_webpack_rails/install_generator.rb
@@ -2,10 +2,10 @@ module ReactWebpackRails
   class InstallGenerator < Rails::Generators::Base
     desc 'Prepare files necessary to use react-webpack-rails gem in a rails app.'
     source_root File.expand_path('../templates', __FILE__)
-    class_option :no_example, type: :boolean, default: false, desc: 'Skip generating example.'
-    class_option :no_server_side, type: :boolean, default: false, desc: 'Skip node server setup.'
-    class_option :no_hot_reload, type: :boolean, default: false, desc: 'Skip hot relaod setup generation.'
-    class_option :no_karma_setup, type: :boolean, default: false, desc: 'Skip karma setup generation.'
+    class_option :example, type: :boolean, default: true, desc: 'Add example'
+    class_option :server_side, type: :boolean, default: true, desc: 'Add node server setup.'
+    class_option :hot_reload, type: :boolean, default: true, desc: 'Add hot relaod setup generation.'
+    class_option :karma_setup, type: :boolean, default: true, desc: 'Add karma setup generation.'
     class_option :react_router, type: :boolean, default: false, desc: 'Install react router'
 
     def generate_core
@@ -13,22 +13,22 @@ module ReactWebpackRails
     end
 
     def generate_hot_reload
-      return if options.no_hot_reload
+      return unless options.hot_reload
       generate 'react_webpack_rails:install:hot_reload --tmp_package'
     end
 
     def generate_server_side
-      return if options.no_hot_reload
+      return unless options.hot_reload
       generate 'react_webpack_rails:install:server_side --tmp_package'
     end
 
     def generate_karma_setup
-      return if options.no_karma_setup
+      return unless options.karma_setup
       generate 'react_webpack_rails:install:karma_setup --tmp_package'
     end
 
     def generate_example
-      return if options.no_example
+      return unless options.example
       example_generator = 'react_webpack_rails:install:example'
       example_generator += ' --no_server_side' if options.no_server_side
       generate example_generator
