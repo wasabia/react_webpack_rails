4dffaff95d85d0da51fac6eb4cf5aa94c7df7d8a
Kacper Golinski <kacper.golinski@netguru.co>
2016-02-12 15:22:51 +0100

    add more tests for redux integrations

--- js/test/integrations/redux.spec.js
+++ js/test/integrations/redux.spec.js
@@ -1,8 +1,15 @@
 import expect, { spyOn } from 'expect';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
+import React from 'react';
 
 import subject from '../../src/integrations/redux';
+
+class AppContainer extends React.Component {
+  render() {
+    return <div>AppContainer</div>;
+  }
+}
 
 const fakeReducer = function() {};
 const validStore = function(initialState) {
@@ -62,6 +69,68 @@ describe('ReduxIntegration', function () {
       subject.mountStore('ValidStore', initialState);
 
       expect(subject.mountedStores.ValidStore).toEqual(validStore(initialState));
+    });
+  });
+
+  describe('#getStore', function() {
+    it('returns undefined if store is not found', function() {
+      expect(subject.getStore('FakeStore')).toBe(undefined);
+    });
+
+    it('returns store by name from mountedStores storage', function() {
+      subject.registerStore('ValidStore', validStore);
+      subject.mountStore('ValidStore', {});
+      expect(subject.getStore('ValidStore')).toEqual(validStore({}));
+    });
+  });
+
+  describe('#registerContainer', function() {
+    it('adds container to the storage', function() {
+      subject.registerContainer('AppContainer', AppContainer);
+
+      expect(subject.containers.AppContainer).toEqual(AppContainer);
+    });
+  });
+
+  describe('#getContainer', function() {
+    it('returns container by name', function() {
+      subject.registerContainer('AppContainer', AppContainer);
+      expect(subject.getContainer('AppContainer')).toEqual(AppContainer);
+    });
+  });
+
+  describe('#createContainer', function () {
+    it('creates redux container', function() {
+      subject.registerContainer('AppContainer', AppContainer);
+      const container = subject.createContainer('AppContainer');
+
+      expect(React.isValidElement(container)).toBe(true);
+      expect(container.type).toBe(AppContainer);
+    });
+  });
+
+  describe('#createRootComponent', function() {
+    it('creates redux root component', function() {
+      subject.registerStore('ValidStore', validStore);
+      const initialState = { fake: 'state' };
+      subject.mountStore('ValidStore', initialState);
+      subject.registerContainer('AppContainer', AppContainer);
+      const rootComponent = subject.createRootComponent('AppContainer', 'ValidStore');
+
+      expect(React.isValidElement(rootComponent)).toBe(true);
+    });
+  });
+
+  describe('#renderContainer', function() {
+    it('calls #createRootComponent and React.render functions', function() {
+      const subjectSpy = spyOn(subject, 'createRootComponent');
+      const reactSpy = spyOn(ReactDOM, 'render');
+
+      subject.renderContainer('ContainerName', 'node', 'StoreName');
+
+      expect(subjectSpy.calls.length).toEqual(1);
+      expect(subjectSpy).toHaveBeenCalledWith('ContainerName', 'StoreName');
+      expect(reactSpy.calls.length).toEqual(1);
     });
   });
 });
